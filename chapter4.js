7a6000577735dd36019faca6c053b52be0385133
lspCoder <15071235801@139.com>
2018-09-16 16:48:56 +0800

    js继承

--- chapter4.js
+++ chapter4.js
@@ -13,17 +13,26 @@ function Cat(name, color) {
     this.color = color;
 }
 
+extend (Cat, Animal);
+
+Cat.prototype.eat = function () {
+    console.log('cat eat');
+}
+
 function extend (Child, Parent) {
     var F = function(){};
     F.prototype = Parent.prototype;
     Child.prototype = new F();
     Child.prototype.constructor = Child;
-    Child.superClass = Parent,prototype;
+    Child.superClass = Parent.prototype;
 }
+
+
 
 var cat1 = new Cat('大黄', '黄色');
 var cat2 = new Cat('大花', '花色');
 
+cat1.eat();
 console.log(cat1.constructor);              //Cat
 console.log(Animal.prototype.constructor == Cat.prototype.constructor);          //false
 console.log(cat1.eat === cat2.eat);              //true
